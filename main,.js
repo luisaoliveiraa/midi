3dcc61a3e12847b98f59d2c6e402095b3843f172
luisaoliveiraa <105437774+luisaoliveiraa@users.noreply.github.com>
2023-10-03 07:35:51 -0300

    sddsdsds

--- main,.js
+++ main,.js
@@ -1,5 +1,7 @@
 function tocaSomPom(){
-    document.querySelector(‘#som_tecla_pom’).play();
+    document.querySelector("#som_tecla_pom").play();
+    
 }
-document.querySelector(‘.tecla_pom’);
-document.querySelector(‘#som_tecla_pom’).play();
+
+document.querySelector(".tecla_pom");
+document.querySelector("#som_tecla_pom").play();
