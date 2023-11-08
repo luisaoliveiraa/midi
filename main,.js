9ecb3d3fdf718ed8695851e6a0bc7bab4296c2f5
luisaoliveiraa <105437774+luisaoliveiraa@users.noreply.github.com>
2023-11-08 11:10:09 -0300

    awketmjrfgfv cf

--- main,.js
+++ main,.js
@@ -44,3 +44,24 @@ function tocaSomClap(){
                                 document.querySelector(".tecla_tom").onclick = tocaSomTom;
 
                                 document.querySelectorAll(".tecla");
+
+                                function tocaSom(idElementoAudio){
+                                    document.querySelector(idElementoAudio).play();
+                                }
+                                
+                                document.querySelector('.tecla_pom').onclick = tocaSom;
+                                
+                                const listaDeTeclas = document.querySelectorAll('.tecla');
+                                
+                                listaDeTeclas[0].onclick = tocaSom;
+                                
+                                for(let contador = 0;contador < listaDeTeclas.length;contador++){
+                                    const tecla = listaDeTeclas[contador];
+                                    const efeito = tecla.classList[1];
+                                    const idAudio = `#som_${efeito}`;
+                                    tecla.onclick = function(){
+                                        tocaSom(idAudio);
+                                    }
+                                    
+                                   
+                                }
